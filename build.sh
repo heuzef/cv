8d1ad6173eb28327c344bd00de23c48f6767afbf
Heuzef <contact@heuzef.com>
2025-07-04 12:45:19 +0200

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 IMAGE=jankapunkt/latexcv:1.0
 CV_FILE_NAME=heuzef_cv
+
+echo "CV generation in progress ..."
+echo ""
 docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" pdflatex --output-directory=$1 $1/"$CV_FILE_NAME".tex
 
-# Create a compressed light weight copy
+echo "Create a compressed light weight copy"
+echo ""
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$1/"$CV_FILE_NAME".min.pdf $1/"$CV_FILE_NAME".pdf
+
+echo "DONE !"
+echo "-----"
+ls -lArth fr | grep "$CV_FILE_NAME"
--- build_AIjusTeX.sh
+++ build_AIjusTeX.sh
@@ -2,14 +2,18 @@
 IMAGE=jankapunkt/latexcv:1.0
 CV_FILE_NAME=heuzef_cv_AIjusTeX
 
-echo "Génération du CV en cours ..."
+echo "CV generation in progress ..."
 echo ""
 
 python3 $1/AIjusTeX.py > $1/"$CV_FILE_NAME".tex
 
-# docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" pdflatex --output-directory=$1 $1/"$CV_FILE_NAME".tex
+# Need PDF files too ? Uncomment the following lines :
 
-# Create a compressed light weight copy
+# docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" pdflatex --output-directory=$1 $1/"$CV_FILE_NAME".tex
+# echo "Create a compressed light weight copy"
+# echo ""
 # gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$1/"$CV_FILE_NAME".min.pdf $1/"$CV_FILE_NAME".pdf
 
-echo "Génération du CV terminée !"
+echo "DONE !"
+echo "-----"
+ls -lArth fr | grep "$CV_FILE_NAME"

65f3513653dcf8f809d36102a072649dac217b68
Heuzef <contact@heuzef.com>
2025-07-04 12:04:31 +0200

    Update build script

--- build_AIjusTeX.sh
+++ build_AIjusTeX.sh
@@ -2,9 +2,14 @@
 IMAGE=jankapunkt/latexcv:1.0
 CV_FILE_NAME=heuzef_cv_AIjusTeX
 
+echo "Génération du CV en cours ..."
+echo ""
+
 python3 $1/AIjusTeX.py > $1/"$CV_FILE_NAME".tex
 
 # docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" pdflatex --output-directory=$1 $1/"$CV_FILE_NAME".tex
 
 # Create a compressed light weight copy
 # gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$1/"$CV_FILE_NAME".min.pdf $1/"$CV_FILE_NAME".pdf
+
+echo "Génération du CV terminée !"

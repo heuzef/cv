a79b05f55be67b27bb60aabb6e2b3c4e8de6e943
Heuzef <contact@heuzef.com>
2026-07-16 21:42:26 +0200

    Update build_AIjusTeX.sh

--- build_AIjusTeX.sh
+++ build_AIjusTeX.sh
@@ -9,11 +9,9 @@ python3 $1/AIjusTeX.py > $1/"$CV_FILE_NAME".tex
 
 # Need PDF files too ? Uncomment the following lines :
 
-# docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" pdflatex --output-directory=$1 $1/"$CV_FILE_NAME".tex
+# docker run --rm -i --net=none -v "$PWD":/data "$IMAGE" pdflatex --output-directory=$1 $1/"$CV_FILE_NAME".tex
 # echo "Create a compressed light weight copy"
 # echo ""
 # gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$1/"$CV_FILE_NAME".min.pdf $1/"$CV_FILE_NAME".pdf
 
 echo "DONE !"
-echo "-----"
-ls -lArth fr | grep "$CV_FILE_NAME"
